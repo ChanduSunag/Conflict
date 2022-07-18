9a3aa54d3038404ba8478f84813f3f3fa5bb0f20
Admin <Admin@DESKTOP-HC1J7FO>
2022-07-18 17:46:10 +0530

    Chandan

---
+++ src/test/java/Smaple/SampleOrganizationTest.java
@@ -0,0 +1,18 @@
+package Smaple;
+
+import org.testng.annotations.Test;
+
+
+public class SampleOrganizationTest {
+
+
+	@Test(groups = "smoke test")
+	public void createOrganizationTest() {
+	System.out.println("=========organization created==========");
+	}
+
+	@Test(groups="regression test")
+	public void editOrganizationTest() {
+	System.out.println("========edit organization========");
+	}
+}
