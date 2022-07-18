9ce7b82e6058ef58e4a56259fdf24b941dcceb5f
asus <asus@DESKTOP-HMSSTKG>
2022-07-18 17:52:38 +0530

    i have changed both test case

--- src/test/java/Smaple/SampleOrganizationTest.java
+++ src/test/java/Smaple/SampleOrganizationTest.java
@@ -9,10 +9,14 @@ public class SampleOrganizationTest {
 	@Test(groups = "smoke test")
 	public void createOrganizationTest() {
 	System.out.println("=========organization created==========");
+	System.out.println("i am lohith");
+	
 	}
 
 	@Test(groups="regression test")
 	public void editOrganizationTest() {
 	System.out.println("========edit organization========");
+	System.out.println("i am lohith");
+	
 	}
 }
